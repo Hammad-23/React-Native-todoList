0a6a8298fb88a27765a0c8b0790b47babeb7cb45
Hammad-23 <hsyed2633@gmail.com>
2020-10-07 04:36:38 -0700

    react todo init

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const[name,setName]= useState()
+  
+
+  // const names= function(e){
+
+  //   const inp = e.target.value
+  //   console.log(inp)
+
+  // }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+      <TextInput  onChangeText={(text)=>setName({text})} placeholder='Write Here'  style={{ height: 40, borderColor: 'gray', borderWidth: 1, maxWidth: 200,minWidth:200 }}/>
+      </View>
+      <View style={{marginTop:20}}>
+        {/* {name.map(function(item){
+          return(
+          <Text style={{marginTop:20}}>{item}</Text>
+          )
+        })} */}
+
+   <Text style={{marginTop:20}}>hammad</Text>
+        
+      </View>
       <StatusBar style="auto" />
     </View>
   );
