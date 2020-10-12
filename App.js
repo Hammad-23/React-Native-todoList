08be1c657c373a3167d8542ec8bedeccb57c4337
Hammad-23 <hsyed2633@gmail.com>
2020-10-12 06:14:07 -0700

    done

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
 
-  const[name,setName]= useState()
+  const[name,setName]= useState([])
+
+  const add = function(){
+
+  }
+
+  // const name = ['hello','hammad','Arif','wasey']
   
 
   // const names= function(e){
@@ -14,22 +20,30 @@ export default function App() {
 
   // }
   return (
+    <ScrollView >
+
+
     <View style={styles.container}>
       <View>
       <TextInput  onChangeText={(text)=>setName({text})} placeholder='Write Here'  style={{ height: 40, borderColor: 'gray', borderWidth: 1, maxWidth: 200,minWidth:200 }}/>
+      <Button onPress={add} title='Add'/>
       </View>
       <View style={{marginTop:20}}>
-        {/* {name.map(function(item){
+        {name.map(function(item){
           return(
-          <Text style={{marginTop:20}}>{item}</Text>
+            <View style={{display: "flex"}}>
+          <Text style={{marginTop:20}}>{item}</Text> 
+          <Button title='Delete'/>
+          </View>
           )
-        })} */}
+        })}
 
-   <Text style={{marginTop:20}}>hammad</Text>
+   {/* <Text style={{marginTop:20}}>hammad</Text> */}
         
       </View>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 
